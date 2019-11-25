bc84d9642ea5e656c584f8df6af6f22e3f2c86e9
jenseneicher-mines <54752181+jenseneicher-mines@users.noreply.github.com>
2019-11-25 10:14:09 -0700

    added xref table and join

--- setup_tables.sql
+++ setup_tables.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS brewery CASCADE;
 DROP TABLE IF EXISTS beer CASCADE;
 DROP TABLE IF EXISTS review CASCADE;
+DROP TABLE IF EXISTS beer_review_xref CASCADE;
 
 
 SELECT 'Make sure you ran load_data.sql before running this script' AS '';
@@ -29,8 +30,16 @@ CREATE TABLE review(
 	appearance NUMERIC(2,1)
 );
 
+
+CREATE TABLE beer_review_xref(
+	beer_id INTEGER REFERENCES Beer(id), 
+	reviewer_name TEXT REFERENCES Review(reviewer_name), 
+	PRIMARY KEY(beer_id, reviewer_name)
+);
+
 -- Move data into tables
 
-INSERT INTO Review (beer_id, rating, aroma, appearance, reviewer_name, palate, taste) SELECT (review_overall, review_aroma, review_appearance, review_profilename, review_palate, review_taste) FROM beer_master;
-INSERT INTO Beer(id, brewery_id, style, name) SELECT (beer_beerid, beer_style, beer_name) FROM beer_master;
 INSERT INTO Brewery(id, name) SELECT (brewery_id, brewery_name) FROM beer_master;
+INSERT INTO Beer(id, brewery_id, style, name) SELECT (beer_beerid, beer_style, beer_name) FROM beer_master;
+INSERT INTO Review (beer_id, rating, aroma, appearance, reviewer_name, palate, taste) SELECT (review_overall, review_aroma, review_appearance, review_profilename, review_palate, review_taste) FROM beer_master;
+INSERT INTO beer_review_xref SELECT beer_id, reviewer_name FROM Beer b JOIN Review R on b.id = R.beer_id;
