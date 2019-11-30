5aed2e52ba7cc72ee28a601385a2548f7b56d397
zsmeton <zsmeton@mines.edu>
2019-11-30 15:22:28 -0700

    Renamed tables for better readablility

--- setup_tables.sql
+++ setup_tables.sql
@@ -1,27 +1,27 @@
-DROP TABLE IF EXISTS breweries CASCADE;
-DROP TABLE IF EXISTS beers CASCADE;
-DROP TABLE IF EXISTS reviews CASCADE;
+DROP TABLE IF EXISTS brewery CASCADE;
+DROP TABLE IF EXISTS beer CASCADE;
+DROP TABLE IF EXISTS review CASCADE;
 
 
-SELECT 'Make sure you ran load_data.sql before running this script' AS '';
+SELECT "Make sure you ran load_data.sql before running this script" AS '' ;
 
 -- Create tables
-CREATE TABLE breweries(
+CREATE TABLE brewery(
 	id INTEGER PRIMARY KEY,
 	name TEXT
 );
 
-CREATE TABLE beers(
+CREATE TABLE beer(
 	id INTEGER PRIMARY KEY,
-	brewery_id INTEGER REFERENCES breweries(id),
+	brewery_id INTEGER REFERENCES brewery(id),
 	name TEXT,
 	style TEXT,
 	abv NUMERIC(3,1)
 );
 
-CREATE TABLE reviews(
+CREATE TABLE review(
 	reviewer_name TEXT,
-	beer_id INTEGER REFERENCES beers(id),
+	beer_id INTEGER REFERENCES beer(id),
 	overall NUMERIC(2,1),
 	aroma NUMERIC(2,1),
 	palate NUMERIC(2,1),
@@ -31,11 +31,11 @@ CREATE TABLE reviews(
 );
 
 -- Move data into tables
-INSERT INTO breweries(id, name) 
+INSERT INTO brewery(id, name) 
 	SELECT DISTINCT brewery_id, brewery_name FROM beer_master;
-INSERT INTO beers(id, brewery_id, name, style, abv)
+INSERT INTO beer(id, brewery_id, name, style, abv)
 	SELECT DISTINCT beer_id, brewery_id, beer_name, beer_style, beer_abv FROM beer_master;
-INSERT INTO reviews(beer_id, overall, aroma, appearance, reviewer_name, palate, taste) 
+INSERT INTO review(beer_id, overall, aroma, appearance, reviewer_name, palate, taste) 
 	SELECT DISTINCT bm1.beer_id, bm1.review_overall, bm1.review_aroma, bm1.review_appearance, 
 	bm1.review_profilename, bm1.review_palate, bm1.review_taste FROM beer_master AS bm1 
 		--WHERE (bm1.review_overall, bm1.review_aroma) 
